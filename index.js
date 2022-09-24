c77e083d5133da1ca18027d611edc5098556fbbc
Robin Behlke <robin@behlke.tech>
2022-09-24 20:10:39 +0200

    Added own logic and stuff

---
+++ index.js
@@ -0,0 +1,21 @@
+const add2Numbers = (a, b) => {
+    return a + b;
+};
+
+const subtract2Numbers = (a, b) => {
+    return a - b;
+};
+
+const multiply2Numbers = (a, b) => {
+    return a * b;
+};
+
+const divide2Numbers = (a, b) => {
+    return a / b;
+};
+
+const werIstBabo = () => {
+    return `xHeitkamp ist Babo!`;
+};
+
+module.exports = { add2Numbers, subtract2Numbers, multiply2Numbers, divide2Numbers, werIstBabo };
